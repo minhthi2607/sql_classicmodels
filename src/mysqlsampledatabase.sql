ee26e8ef844c2363654d67add08a95d43b245af2
Phan Minh Thi <thi@corsivalab.space>
2026-03-03 09:55:24 +0700

    update

--- src/mysqlsampledatabase.sql
+++ src/mysqlsampledatabase.sql
@@ -1,4 +1,4 @@
-git /*
+/*
   Name: MySQL Sample Database classicmodels
   Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
 */
@@ -156,21 +156,94 @@ alter table customers add index idx_fullname(contactfirstname, contactlastname);
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 -- alter table customers drop index idx_fullname;
 
+-- DELIMITER //
+-- create procedure  findallcustomer()
+-- begin 
+-- select * from customers;
+-- end //
+-- DELIMITER ;
+
+-- call findallcustomer();
+
+-- DELIMITER //
+-- drop procedure if exists `findallcustomers` //
+-- create procedure findallcustomers()
+-- begin 
+-- select * from customers where customerNumber = 175;
+-- end //
+-- delimiter ;
+
+-- call findallcustomers();
+
+
+-- DELIMITER //
+
+-- CREATE PROCEDURE getCusById
+
+-- (IN cusNum INT(11))
+
+-- BEGIN
+
+--   SELECT * FROM customers WHERE customerNumber = cusNum;
+
+-- END //
+
+-- DELIMITER ;
+
+-- call getCusById(175);
+
 DELIMITER //
-create procedure  findallcustomer()
-begin 
-select * from customers;
-end //
+
+CREATE PROCEDURE GetCustomersCountByCitys(
+
+    IN  in_city VARCHAR(50),
+
+    OUT total INT
+
+)
+
+BEGIN
+
+    SELECT COUNT(customerNumber)
+
+    INTO total
+
+    FROM customers
+
+    WHERE city = in_city;
+
+END//
+
 DELIMITER ;
 
-call findallcustomer();
+CALL GetCustomersCountByCitys('Lyon',@total);
+
+SELECT @total;
 
 DELIMITER //
-drop procedure if exists `findallcustomers` //
-create procedure findallcustomers()
-begin 
-select * from customers where customerNumber = 175;
-end //
-delimiter ;
 
-call findallcustomers();
+CREATE PROCEDURE SetCounter(
+
+    INOUT counter INT,
+
+    IN inc INT
+
+)
+
+BEGIN
+
+    SET counter = counter + inc;
+
+END//
+
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1); -- 2
+
+CALL SetCounter(@counter,1); -- 3
+
+CALL SetCounter(@counter,5); -- 8
+
+SELECT @counter; -- 8
